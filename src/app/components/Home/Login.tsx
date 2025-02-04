c556742056ac70df455d684c3e52e29bcc95c499
Rimshasultan11 <rimshasultan@gmail.com>
2025-02-05 03:04:00 +0500

    changes

--- src/app/components/Home/Login.tsx
+++ src/app/components/Home/Login.tsx
@@ -1,13 +1,32 @@
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+// import React from "react";
+// import { FaUserCircle } from "react-icons/fa";
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <SignedOut>
+//         <SignInButton>
+//           <FaUserCircle className=" text-2xl cursor-pointer" />
+//         </SignInButton>
+//       </SignedOut>
+//       <SignedIn>
+//         <UserButton />
+//       </SignedIn>
+//     </div>
+//   );
+// };
+
+// export default Login;
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 
 const Login = () => {
   return (
     <div>
       <SignedOut>
-        <SignInButton>
-          <FaUserCircle className=" text-2xl cursor-pointer" />
+        <SignInButton mode="modal">
+          <FaUserCircle className="text-2xl cursor-pointer" />
         </SignInButton>
       </SignedOut>
       <SignedIn>
