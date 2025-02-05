486acd5d808505981c74a2bd4bae7c948b7918df
Rimshasultan11 <rimshasultan@gmail.com>
2025-02-05 16:56:26 +0500

    changes

--- src/app/components/Home/Login.tsx
+++ src/app/components/Home/Login.tsx
@@ -1,63 +1,10 @@
-// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-// import React from "react";
-// import { FaUserCircle } from "react-icons/fa";
-
-// const Login = () => {
-//   return (
-//     <div>
-//       <SignedOut>
-//         <SignInButton>
-//           <FaUserCircle className=" text-2xl cursor-pointer" />
-//         </SignInButton>
-//       </SignedOut>
-//       <SignedIn>
-//         <UserButton />
-//       </SignedIn>
-//     </div>
-//   );
-// };
-
-// export default Login;
-// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-// import { FaUserCircle } from "react-icons/fa";
-
-// const Login = () => {
-//   return (
-//     <div>
-//       <SignedOut>
-//         <SignInButton mode="modal">
-//           <FaUserCircle className="text-3xl cursor-pointer" />
-//         </SignInButton>
-//       </SignedOut>
-//       <SignedIn>
-//         <UserButton />
-//       </SignedIn>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-"use client";
-
-import { useAuth, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 
 const Login = () => {
-  const { isSignedIn } = useAuth(); // Clerk Authentication State
-
   return (
     <div>
-      {!isSignedIn ? (
-        <SignInButton mode="modal">
-          <FaUserCircle className="text-3xl cursor-pointer" />
-        </SignInButton>
-      ) : (
-        <UserButton />
-      )}
+      <FaUserCircle className=" text-2xl cursor-pointer" />
     </div>
   );
 };
-
-export default Login;
-
