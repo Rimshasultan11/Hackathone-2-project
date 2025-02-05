0456693bf10494e287eb35a648f2785ad2243d52
Rimshasultan11 <rimshasultan@gmail.com>
2025-02-05 16:10:16 +0500

    changes

--- src/app/components/Home/ClientWrapper.tsx
+++ src/app/components/Home/ClientWrapper.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { useState, useEffect } from "react";
 
 export default function ClientWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null; // Wait until mounted
+
   return <ClerkProvider>{children}</ClerkProvider>;
 }
