bb4778e297562a44cf68f47371a2830222f0cf20
Rimshasultan11 <rimshasultan@gmail.com>
2025-02-05 14:45:35 +0500

    changes

--- src/app/components/Home/Login.tsx
+++ src/app/components/Home/Login.tsx
@@ -18,22 +18,46 @@
 // };
 
 // export default Login;
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+// import { FaUserCircle } from "react-icons/fa";
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <SignedOut>
+//         <SignInButton mode="modal">
+//           <FaUserCircle className="text-3xl cursor-pointer" />
+//         </SignInButton>
+//       </SignedOut>
+//       <SignedIn>
+//         <UserButton />
+//       </SignedIn>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+"use client";
+
+import { useAuth, SignInButton, UserButton } from "@clerk/nextjs";
 import { FaUserCircle } from "react-icons/fa";
 
 const Login = () => {
+  const { isSignedIn } = useAuth(); // Clerk Authentication State
+
   return (
     <div>
-      <SignedOut>
+      {!isSignedIn ? (
         <SignInButton mode="modal">
           <FaUserCircle className="text-3xl cursor-pointer" />
         </SignInButton>
-      </SignedOut>
-      <SignedIn>
+      ) : (
         <UserButton />
-      </SignedIn>
+      )}
     </div>
   );
 };
 
 export default Login;
+
